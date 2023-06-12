ae0e970ba5e4c54996a54fc413f4915cc0db9e14
Darshan Chhetri <mitch4827@gmail.com>
2023-06-12 10:23:47 +0545

    Animation Added

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,10 @@ export default function Home() {
     <div className={styles.container}>
       <div className={styles.item}>
         <h1 className={styles.title}>Better Design For Your Digital Product</h1>
-        <p className={styles.desc}>Turning your idea in reality. We bring together the teams from the global tech technology.</p>
+        <p className={styles.desc}>
+          Turning your idea in reality. We bring together the teams from the
+          global tech technology.
+        </p>
         <Button url="/portfolio" text="See Our Works" />
       </div>
       <div className={styles.item}>
