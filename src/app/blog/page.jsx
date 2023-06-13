2f50a004f5f244df6e657b75649d6bc0bb2cb719
Darshan Chhetri <mitch4827@gmail.com>
2023-06-13 13:11:55 +0545

    Blog page ui completed

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,10 +1,71 @@
 import React from 'react'
 import styles from './page.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Blog = () => {
   return (
-    <div className={styles.container}>
-      Blog Page
+    <div className={styles.mainContainer}>
+        <Link href="/blog/testId" className={styles.container}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="/websites.jpg"
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>Test</h1>
+            <p className={styles.desc}>Desc</p>
+          </div>
+        </Link>
+        <Link href="/blog/testId" className={styles.container}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="/websites.jpg"
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>Test</h1>
+            <p className={styles.desc}>Desc</p>
+          </div>
+        </Link>
+        <Link href="/blog/testId" className={styles.container}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="/websites.jpg"
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>Test</h1>
+            <p className={styles.desc}>Desc</p>
+          </div>
+        </Link>
+        <Link href="/blog/testId" className={styles.container}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="/websites.jpg"
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>Test</h1>
+            <p className={styles.desc}>Desc</p>
+          </div>
+        </Link>
     </div>
   )
 }
