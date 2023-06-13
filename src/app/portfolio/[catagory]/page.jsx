082062127c852ba7be3d2f32c5006e0c06b4efa1
Darshan Chhetri <mitch4827@gmail.com>
2023-06-13 12:57:21 +0545

    Portfolio catagory page ui

--- src/app/portfolio/[catagory]/page.jsx
+++ src/app/portfolio/[catagory]/page.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import React from "react";
+import styles from "./page.module.css";
+import Button from "@/components/button/Button";
+import Image from "next/image";
 
-const  Catagory = () => {
+const Catagory = ({ params }) => {
   return (
-    <div>
-      Catagory
+    <div className={styles.container}>
+      <h1 className={styles.cataTitle}>{params.catagory}</h1>
+      <div className={styles.item}>
+        <div className={styles.contant}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button url="#" text="See More" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/apps.jpg"
+            fill={true}
+            alt="image"
+            className={styles.img}
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.contant}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button url="#" text="See More" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/apps.jpg"
+            fill={true}
+            alt="image"
+            className={styles.img}
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.contant}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button url="#" text="See More" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/apps.jpg"
+            fill={true}
+            alt="image"
+            className={styles.img}
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default  Catagory
+export default Catagory;
